677457a90a604c127f9da28f609fc9082618bf78
hgs-study <hgstudy_@naver.com>
2021-08-14 17:34:42 +0900

    Level_2.Hash (위장)

--- src/com/level_02/Hash.java
+++ src/com/level_02/Hash.java
@@ -1,6 +1,9 @@
 package com.level_02;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Hash {
 
@@ -16,7 +19,7 @@ public class Hash {
     //어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
 
     //문제 100%, 효율성 0%
-    public boolean solution(String[] phone_book) {
+    public boolean solution1_1(String[] phone_book) {
         boolean answer = true;
 
         HashMap<String,Object> map = new HashMap<String, Object>();
@@ -40,7 +43,7 @@ public class Hash {
     }
 
     //문제 100%, 효율성 100%
-    public boolean solution1(String[] phone_book) {
+    public boolean solution1_2(String[] phone_book) {
         HashMap<String,Object> map = new HashMap<String, Object>();
         for (String s : phone_book) {
             map.put(s, 0);
@@ -53,6 +56,40 @@ public class Hash {
         }
         return true;
     }
+
+
+
+    //문제 설명
+    //스파이들은 매일 다른 옷을 조합하여 입어 자신을 위장합니다.
+    //
+    //예를 들어 스파이가 가진 옷이 아래와 같고 오늘 스파이가 동그란 안경, 긴 코트, 파란색 티셔츠를 입었다면
+    //다음날은 청바지를 추가로 입거나 동그란 안경 대신 검정 선글라스를 착용하거나 해야 합니다.
+    //
+    //종류	이름
+    //얼굴	동그란 안경, 검정 선글라스
+    //상의	파란색 티셔츠
+    //하의	청바지
+    //겉옷	긴 코트
+    //
+    //스파이가 가진 의상들이 담긴 2차원 배열 clothes가 주어질 때 서로 다른 옷의 조합의 수를 return 하도록 solution 함수를 작성해주세요.
+    public int solution2_1(String[][] clothes) {
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        int answer = 1;
+
+        for (int i = 0; i < clothes.length; i++) {
+            if(map.containsKey(clothes[i][1]))
+                map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
+            else
+                map.put(clothes[i][1], 1);
+        }
+
+        for (Iterator<Integer> i = map.values().iterator(); i.hasNext(); )
+            answer *= i.next() +1;
+
+        return answer - 1;
+    }
+
+
 
 
 }
