20afb8a0cc1d5625e76bd31f64180f8a35cf7ec0
hgs-study <hgstudy_@naver.com>
2021-09-17 18:10:22 +0900

    [Baekjoon] Greedy - 10162번, 전자레인지 (조건 추가)

--- src/com/baekjoon/greedy/Practice11.java
+++ src/com/baekjoon/greedy/Practice11.java
@@ -5,28 +5,34 @@ import java.util.Scanner;
 
 // 10162번, 전자레인지
 public class Practice11 {
-
     public static void main(String[] args) {
         Scanner scanner =  new Scanner(System.in);
         int total = scanner.nextInt();
-        int sum = total;
         int[] change = new int[3];
 
-        if(500 <= total) {
+        if(total >= 300) {
             change[0] += total / 300;
             total %= 300;
         }
-        if(60 <= total) {
+
+        if(total >= 60) {
             change[1] += total / 60;
             total %= 60;
         }
 
-        change[2] += total / 10;
-        total %= 10;
+        if(total >= 10){
+            change[2] += total / 10;
+            total %= 10;
+        }
 
-        if(change[0]*500 + change[1]*60 + change[2]*10 == sum )
-            System.out.print(change[0] + " " + change[1] + " " +change[2] );
-        else
-            System.out.print(-1);
+
+        if(total == 0 ){
+            for (int i = 0; i < change.length; i++) {
+                System.out.print(change[i] + " ");
+            }
+        }
+        else{
+            System.out.print("-1");
+        }
     }
 }
