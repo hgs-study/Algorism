3cc9390dc033aa709a3ef76be38c052b18798624
hgs-study <hgstudy_@naver.com>
2021-09-29 14:18:04 +0900

    [Baekjoon] DFS - 2606번, 바이러스

--- src/com/baekjoon/dfs/DFS.java
+++ src/com/baekjoon/dfs/DFS.java
@@ -1,7 +1,11 @@
 package com.baekjoon.dfs;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
+// 2606번, 바이러스
 public class DFS {
 
 
@@ -10,30 +14,44 @@ public class DFS {
     //
     //예를 들어 7대의 컴퓨터가 <그림 1>과 같이 네트워크 상에서 연결되어 있다고 하자. 1번 컴퓨터가 웜 바이러스에 걸리면 웜 바이러스는 2번과 5번 컴퓨터를 거쳐 3번과 6번 컴퓨터까지 전파되어 2, 3, 5, 6 네 대의 컴퓨터는 웜 바이러스에 걸리게 된다. 하지만 4번과 7번 컴퓨터는 1번 컴퓨터와 네트워크상에서 연결되어 있지 않기 때문에 영향을 받지 않는다.
     //어느 날 1번 컴퓨터가 웜 바이러스에 걸렸다. 컴퓨터의 수와 네트워크 상에서 서로 연결되어 있는 정보가 주어질 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 출력하는 프로그램을 작성하시오.
-    public static void main(String[] args) {
-        int[][] computers = {{7},{6},{1,2},{2,3},{1,5},{5,2},{5,6},{4,7}};
-        boolean[] visited = new boolean[computers.length];
-        int count = 1;
+    static int N;
+    static int M;
+    static boolean arr[][];
+    static boolean visited[];
+    static int result;
 
-        for (int i = 2; i < computers.length; i++) {
-            if(!visited[i]) {
-                dfs(computers, visited, i);
-                count++;
+    public static void dfs(int idx) {
+        visited[idx] = true;
+
+        for (int i = 1; i <= N; i++) {
+            if (arr[idx][i] && !visited[i]) {
+                result++;
+                dfs(i);
             }
         }
-        System.out.println(count);
     }
 
-    public static void dfs(int[][] computers, boolean[] visited, int index ){
-        visited[index] = true;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        for (int i = 0; i < computers[index].length ; i++) {
-            if(computers[index][i] == computers[0][0])
-                return;
-            if(!visited[computers[index][i] + 1]) {
-                dfs(computers, visited, index +1);
-            }
+        N = Integer.parseInt(br.readLine());
+        arr = new boolean[N+1][N+1];
+        visited = new boolean[N+1];
+        result = 0;
+
+        M = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            arr[start][end] = true;
+            arr[end][start] = true;
         }
 
+        dfs(1);
+
+        System.out.println(result);
     }
 }
