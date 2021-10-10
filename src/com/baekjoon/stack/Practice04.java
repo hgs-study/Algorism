7a1d3581a37ecb0f7842e7c45b9f6d96359dcfa6
hgs-study <hgstudy_@naver.com>
2021-10-10 17:49:48 +0900

    [Baekjoon] Stack - 17608번, 막대기

---
+++ src/com/baekjoon/stack/Practice04.java
@@ -0,0 +1,33 @@
+package com.baekjoon.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// 17608번, 막대기
+public class Practice04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            int temp = sc.nextInt();
+
+            while (!stack.empty() && isVisible(stack, temp)) {
+                stack.pop();
+            }
+            stack.push(temp);
+        }
+
+        System.out.println(stack.size());
+    }
+
+    // 현재 막대기의 길이가 stack의 top에 들어있는 막대기보다 크거나 같아서
+    // 왼쪽에서 봤을 때 막대기가 보이는지 여부를 리턴하는 함수
+    public static boolean isVisible(Stack stack, int temp) {
+        int top = (int) stack.peek();
+
+        return temp >= top;
+    }
+}
